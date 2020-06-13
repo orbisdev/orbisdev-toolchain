a4a6cea12c2dc7edd5a8e6d5669bfc7767593b61
Francisco Javier Trujillo Mata <fjtrujy@gmail.com>
2020-06-13 16:42:36 +0200

    Add stage1, clang compilation

--- stage0-binutils.sh
+++ stage0-binutils.sh
@@ -1,12 +1,14 @@
-#±/bin/bash
+#/bin/bash
 set -e
 
 ROOT=${PWD}
-PS4SDK=$ROOT/toolchain_temp
+PS4TOOLCHAIN=${ROOT}/toolchain
 PROC_NR=$(getconf _NPROCESSORS_ONLN)
+
 git clone --depth 1 https://github.com/bminor/binutils-gdb
 cd binutils-gdb
-./configure --prefix=$PS4SDK --target="x86_64-pc-freebsd9" \
+
+./configure --prefix=${PS4TOOLCHAIN} --target="x86_64-pc-freebsd9" \
 	--disable-nls \
 	--disable-dependency-tracking \
 	--disable-werror \
@@ -14,8 +16,8 @@ cd binutils-gdb
 	--enable-lto \
 	--enable-plugins \
 	--enable-poison-system-directories
-make -j $PROC_NR
+make -j ${PROC_NR}
 make install
 
-cd $PS4SDK/bin
+cd ${PS4TOOLCHAIN}/bin
 ln -s x86_64-pc-freebsd9-ld orbis-ld
--- toolchain.sh
+++ toolchain.sh
@@ -1,8 +1,8 @@
-#±/bin/bash
+#/bin/bash
 set -e
 
-sh stage0-binutils.sh
-# sh stage1-musl.sh $1
+# sh stage0-binutils.sh
+sh stage1-clang.sh
 # sh stage2-libcxx.sh
 # sh stage3-libgen.sh
 # sh stage4-ps4sdk-headers.sh
