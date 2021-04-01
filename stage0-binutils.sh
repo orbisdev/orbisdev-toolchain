517dfe2165dffbefb96f8704e3911bfeea0d1ce1
Francisco Javier Trujillo Mata <fjtrujy@gmail.com>
2021-04-01 23:24:28 +0200

    Improve binutils compilation

--- stage0-binutils.sh
+++ stage0-binutils.sh
@@ -3,8 +3,15 @@ set -e
 
 PROC_NR=$(getconf _NPROCESSORS_ONLN)
 
-git clone --depth 1 https://github.com/bminor/binutils-gdb
-cd binutils-gdb
+## Download the source code.
+REPO_URL="https://github.com/bminor/binutils-gdb"
+REPO_FOLDER="binutils-gdb"
+BRANCH_NAME="binutils-2_36_1"
+if test ! -d "$REPO_FOLDER"; then
+	git clone --depth 1 -b $BRANCH_NAME $REPO_URL && cd $REPO_FOLDER || exit 1
+else
+	cd $REPO_FOLDER && git fetch origin && git reset --hard origin/${BRANCH_NAME} || exit 1
+fi
 
 ./configure --prefix=${ORBISDEV} --target="x86_64-pc-freebsd9" \
 	--disable-nls \
@@ -15,5 +22,9 @@ cd binutils-gdb
 	--enable-plugins \
 	--enable-poison-system-directories  \
 	--program-prefix=orbis-
-make -j ${PROC_NR} CFLAGS="$CFLAGS -O2" LDFLAGS="$LDFLAGS -s"
-make install -j ${PROC_NR}
+
+## Compile and install.
+make --quiet -j $PROC_NR clean   || { exit 1; }
+make --quiet -j $PROC_NR || { exit 1; }
+make --quiet -j $PROC_NR install-strip || { exit 1; }
+make --quiet -j $PROC_NR clean   || { exit 1; }
