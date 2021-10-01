f4784d494ff46f4a9c7c70a4cc926341b3220636
Francisco Javier Trujillo Mata <fjtrujy@gmail.com>
2021-10-01 11:23:53 +0200

    Update Binutils version and script

--- stage0-binutils.sh
+++ stage0-binutils.sh
@@ -6,22 +6,24 @@ PROC_NR=$(getconf _NPROCESSORS_ONLN)
 ## Download the source code.
 REPO_URL="https://github.com/bminor/binutils-gdb"
 REPO_FOLDER="binutils-gdb"
-BRANCH_NAME="binutils-2_36_1"
+BRANCH_NAME="binutils-2_37"
 if test ! -d "$REPO_FOLDER"; then
 	git clone --depth 1 -b $BRANCH_NAME $REPO_URL && cd $REPO_FOLDER || exit 1
 else
 	cd $REPO_FOLDER && git fetch origin && git reset --hard origin/${BRANCH_NAME} || exit 1
-fi
+fi	
+	
+TARGET="x86_64-pc-freebsd9"
+TARG_XTRA_OPTS=""
 
-./configure --prefix=${ORBISDEV} --target="x86_64-pc-freebsd9" \
-	--disable-nls \
-	--disable-dependency-tracking \
-	--disable-werror \
-	--enable-ld \
-	--enable-lto \
-	--enable-plugins \
-	--enable-poison-system-directories  \
-	--program-prefix=orbis-
+## Configure the build.
+./configure \
+  --quiet \
+  --prefix="${ORBISDEV}" \
+  --target="$TARGET" \
+  --disable-sim \
+  --disable-nls \
+  $TARG_XTRA_OPTS || { exit 1; }
 
 ## Compile and install.
 make --quiet -j $PROC_NR clean   || { exit 1; }
